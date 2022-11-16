c35953f4c758c6b93df31040bb12d0bc1250b947
Mohammad Shaikh <94097592+shaikhmd007@users.noreply.github.com>
2022-11-16 11:34:02 +0530

    Find_Gits_Project Uploaded

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <div className="card footer">
+      <h2>FindGits | by shaikh_md | &copy; 2022</h2>
+      <a href="#" target="_blank" rel="noreferrer">
+        Github Source Code
+      </a>
+      <p> Made with ❤️ &amp; 💻</p>
+    </div>
+  );
+};
+
+export default Footer;
