c881efc5253b6c9dfd8e15cad315542258bc7c59
Mohammad Shaikh <94097592+shaikhmd007@users.noreply.github.com>
2022-11-16 11:34:55 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="card footer">
       <h2>FindGits | by shaikh_md | &copy; 2022</h2>
-      <a href="#" target="_blank" rel="noreferrer">
+      <a href="https://github.com/shaikhmd007/Find_Gits" target="_blank" rel="noreferrer">
         Github Source Code
       </a>
       <p> Made with ❤️ &amp; 💻</p>
